a315cdb5d7bc78e747a5331252d558fc67e753fd
alizs10 <ali.text77@gmail.com>
2024-11-24 14:23:33 +0330

    fix: expense preview

--- components/Dashboard/Event/ExpensePreview.tsx
+++ components/Dashboard/Event/ExpensePreview.tsx
@@ -13,8 +13,8 @@ type PropsTypes = {
 function ExpensePreview({ type, desc, amount, payer, group, to, from }: PropsTypes) {
 
     return (
-        <div className="mx-5 px-5 py-2 items-center rounded-xl bg-indigo-100 text-indigo-900 flex gap-x-4 text-sm">
-            <Zap className="size-4" />
+        <div className="mx-5 px-5 py-2 items-center rounded-xl bg-indigo-100 text-indigo-900 text-sm">
+            <Zap className="size-3.5 mt-1 ml-1 float-right" />
             {type === 'expend' ? (
                 <span>
                     {payer} برای {group?.length} نفر به دلیل {desc} مقدار {amount} تومان هزینه کرده است.
